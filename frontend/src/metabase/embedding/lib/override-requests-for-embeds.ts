8aa059629f4d916a7cbfa7498c95f752e493a95a
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-19 13:13:05 +0300

    Avoid cast on method

--- frontend/src/metabase/embedding/lib/override-requests-for-embeds.ts
+++ frontend/src/metabase/embedding/lib/override-requests-for-embeds.ts
@@ -1,4 +1,5 @@
 import { sessionPropertiesPath } from "metabase/api";
+import type { RequestMethod } from "metabase/api/legacy-client";
 import {
   PLUGIN_API,
   PLUGIN_CONTENT_TRANSLATION,
@@ -45,7 +46,7 @@ const EMBED_URL_TRANSFORMATIONS: Record<
   string,
   (data: { embedType: EmbedType }) => {
     url: string;
-    method: "GET" | "POST";
+    method: RequestMethod;
   }
 > = {
   [URL_PATTERNS.CARD_QUERY]: ({ embedType }) => ({
@@ -83,7 +84,7 @@ const EMBED_URL_TRANSFORMATIONS: Record<
 } as const;
 
 type RequestData = {
-  method: "GET" | "POST";
+  method: RequestMethod;
   url: string;
   options: {
     headers?: Record<string, string>;
