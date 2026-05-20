0c99be5b133202a043babc7f80e771543e3d595a
Mike Appleby <86076+appleby@users.noreply.github.com>
2026-05-19 18:30:00 -0700

    Fix lint warning about with-redefs

--- test/metabase/metabot/persistence_test.clj
+++ test/metabase/metabot/persistence_test.clj
@@ -734,7 +734,7 @@
                                           conversation-id
                                           "transforms_codegen"
                                           {:role "user" :content "make a SQL transform"})]
-          (with-redefs [nqa/tables-for-native (fn [_ & _] {:tables [{:table-id orders-id}]})]
+          (mt/with-dynamic-fn-redefs [nqa/tables-for-native (fn [_ & _] {:tables [{:table-id orders-id}]})]
             (metabot-persistence/finalize-assistant-turn!
              conversation-id
              assistant-msg-id
